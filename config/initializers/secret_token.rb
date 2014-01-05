f84f25a5455ccceb3515dc7769b678ef9d76d770
Kevin Curtis <foilking21@gmail.com>
2014-01-05 11:01:47 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Readit::Application.config.secret_key_base = '2ce195bdb28b527b2dafa11f360a0ca7b28fbf94965c32380bcad783d176a76f1e2af8ba62a8a4e467bf7afd8ab2569d267e66bbef96f2f0063c7bbb961f865f'
